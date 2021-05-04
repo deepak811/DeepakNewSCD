148d183f3c50bbcc01b58908cc4b8d7a1d9ef0a9
deepak.badgotya <deepak.badgotya@Win10-SPBA33.sp.com>
2021-05-04 16:34:12 +0530

    new project to github

---
+++ src/test/java/runner/Runner.java
@@ -0,0 +1,24 @@
+package runner;
+
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+
+	@RunWith(Cucumber.class)
+	@CucumberOptions
+	(
+	features="C:/Users/Deepak.Badgotya/eclipse-workspace/NewSCD/src/test/java/feature/NewSCDLoginFeature.feature",
+	glue= {"stepdefinition"},
+	plugin = ("json:target/cucumber-reports/CucumberTestReport.json"),
+	tags="@Smoketest1",
+			//tags="@Smoketest1 or @Smoketest2 or @Smoketest6", 
+				//tags="@Smoketest1 or @Smoketest2 or @Smoketest7", 
+			//@Smoketest2 or @Smoketest3 or @Smoketest4 or @Smoketest5
+	monochrome = true
+			)
+		
+	public class Runner {
+
+}
