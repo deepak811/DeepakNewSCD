fb8edbd67a42cc790e4f2a62a5e6643703e572bc
deepak.badgotya <deepak.badgotya@Win10-SPBA33.sp.com>
2021-05-07 12:48:31 +0530

    do

--- src/test/java/runner/Runner.java
+++ src/test/java/runner/Runner.java
@@ -12,6 +12,7 @@ import io.cucumber.junit.CucumberOptions;
 	features="C:/Users/Deepak.Badgotya/eclipse-workspace/NewSCD/src/test/java/feature/NewSCDLoginFeature.feature",
 	glue= {"stepdefinition"},
 	plugin = ("json:target/cucumber-reports/CucumberTestReport.json"),
+			publish = true,
 	tags="@Smoketest1 or @Smoketest2",
 			//tags="@Smoketest1 or @Smoketest2 or @Smoketest6", 
 				//tags="@Smoketest1 or @Smoketest2 or @Smoketest7", 
