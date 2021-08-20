f6d0d3d58619c3149a923e7b4664592a9fd86cfe
deepak.badgotya <deepak.badgotya@Win10-SPBA33.sp.com>
2021-08-20 13:08:27 +0530

    hi

--- src/test/java/runner/Runner.java
+++ src/test/java/runner/Runner.java
@@ -27,7 +27,7 @@ import io.cucumber.junit.CucumberOptions;
 			//tags="@Smoketest1 or @Smoketest2 or @Smoketest7",
 	
 			//4)
-			tags="@Smoketest1 or @Smoketest5 or @Smoketest8",
+			tags="@Smoketest1 or @Smoketest5 or @Smoketest9",
 			publish = true,
 			
 			//@Smoketest2 or @Smoketest3 or @Smoketest4 or @Smoketest5
